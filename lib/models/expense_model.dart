ed2f515591007e678688e77750c3a1404e2d1a45
Silva3012 <silvano.n92@gmail.com>
2023-11-13 15:28:35 +0200

    Format the date and icons

--- lib/models/expense_model.dart
+++ lib/models/expense_model.dart
@@ -1,8 +1,19 @@
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
 
 const uuid = Uuid();
 
+final formatter = DateFormat.yMd();
+
 enum Category { food, travel, leisure, work }
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
 
 class Expense {
   Expense(
@@ -17,4 +28,9 @@ class Expense {
   final double amount;
   final DateTime date;
   final Category category;
+
+  // a getter to format the date
+  String get formattedDate {
+    return formatter.format(date);
+  }
 }
