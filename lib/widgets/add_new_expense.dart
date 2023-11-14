4876aee7789b6b35e96b54b24d130caff396f5d1
Silva3012 <silvano.n92@gmail.com>
2023-11-14 20:31:41 +0200

    Add amount, date picker, save and cancel buttons to the modal

--- lib/widgets/add_new_expense.dart
+++ lib/widgets/add_new_expense.dart
@@ -10,17 +10,90 @@ class AddNewExpense extends StatefulWidget {
 }
 
 class _AddNewExpenseState extends State<AddNewExpense> {
+  final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
+  // Close the modal
+  void _closeModal() {
+    Navigator.pop(context);
+  }
+
+  // Function for date picker
+  void _currentDatePicker() {
+    final today = DateTime.now();
+    final firstDate = DateTime(today.year - 1, today.month, today.day);
+    showDatePicker(
+        context: context,
+        initialDate: today,
+        firstDate: firstDate,
+        lastDate: today);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
+    return Padding(
+      padding: const EdgeInsets.all(16),
       child: Column(children: [
         TextField(
+          controller: _titleController,
           maxLength: 50,
-          decoration: InputDecoration(
+          decoration: const InputDecoration(
             label: Text("Enter your expense title"),
           ),
-        )
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: TextField(
+                keyboardType: TextInputType.number,
+                controller: _amountController,
+                decoration: const InputDecoration(
+                  label: Text("Amount"),
+                  prefixText: "R ",
+                ),
+              ),
+            ),
+            const SizedBox(width: 16),
+            Expanded(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  const Text("Select Date"),
+                  IconButton(
+                    onPressed: _currentDatePicker,
+                    icon: const Icon(Icons.calendar_month),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+        Row(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                print(_titleController.text);
+                print(_amountController.text);
+                // _closeModal();
+              },
+              child: const Text("Save"),
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            OutlinedButton(
+              onPressed: _closeModal,
+              child: const Text("Cancel"),
+            ),
+          ],
+        ),
       ]),
     );
   }
