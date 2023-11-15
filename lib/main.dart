7c1b708769233f33c49bc34f2dfaf9abfa4f32e4
Silva3012 <silvano.n92@gmail.com>
2023-11-15 16:18:30 +0200

    Set up text theme

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,29 @@ void main() {
         useMaterial3: true,
         colorScheme: kColorScheme,
         appBarTheme: const AppBarTheme().copyWith(
-          backgroundColor: kColorScheme.onPrimary,
-          foregroundColor: kColorScheme.onPrimaryContainer,
+          backgroundColor: kColorScheme.primary,
+          foregroundColor: kColorScheme.primaryContainer,
         ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.inversePrimary,
+          margin: const EdgeInsets.all(8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer),
+        ),
+        outlinedButtonTheme: OutlinedButtonThemeData(
+          style: OutlinedButton.styleFrom(
+            backgroundColor: kColorScheme.onSecondary,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: kColorScheme.onSecondaryContainer,
+                fontSize: 18,
+              ),
+            ),
       ),
       home: const Expenses(),
     ),
